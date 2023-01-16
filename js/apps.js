a3b2df6048cdc317136ef6c3c4322debdaf747a2
Sohel Rana <expartdesigns5@gmail.com>
2023-01-16 23:54:38 +0600

    first commit

---
+++ js/apps.js
@@ -0,0 +1,16 @@
+
+// nav section start
+let btn1 = document.querySelector(".btn1")
+let dropdown = document.querySelector(".dropdown")
+let down = document.querySelector(".fa-angle-down")
+
+btn1.addEventListener("click", function(){
+    if( dropdown.style.display == "block"){
+        dropdown.style.display = "none"
+        down.style.transform ="rotate(0deg)"
+    }else{
+        dropdown.style.display ="block"
+        down.style.transform ="rotate(180deg)"
+    }
+})
+// nav section end
